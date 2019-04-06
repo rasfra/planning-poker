0afe82a621b2150d8f30f59f0a1eece6d4990344
rasfra <rasmus.franke@gmail.com>
2019-04-06 12:34:21 +0200

    Initial commit

---
+++ src/main/kotlin/se/franke/planningpoker/PlanningPokerApplication.kt
@@ -0,0 +1,11 @@
+package se.franke.planningpoker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanningPokerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanningPokerApplication>(*args)
+}
---
+++ src/test/kotlin/se/franke/planningpoker/PlanningPokerApplicationTests.kt
@@ -0,0 +1,16 @@
+package se.franke.planningpoker
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
