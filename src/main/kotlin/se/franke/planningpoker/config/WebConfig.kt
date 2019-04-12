0c99405180f6c4f4081e20d20052341db905c80a
rasfra <rasmus.franke@gmail.com>
2019-04-12 18:33:54 +0200

    Forward requests to react router

--- src/main/kotlin/se/franke/planningpoker/config/WebConfig.kt
+++ src/main/kotlin/se/franke/planningpoker/config/WebConfig.kt
@@ -2,15 +2,24 @@ package se.franke.planningpoker.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+    }
+
+    // Forwards requests to react router
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/")
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/")
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/")
     }
 }
